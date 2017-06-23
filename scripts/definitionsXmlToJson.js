7f12b8f17258e246b2c5cdba9774ad18930d0840
eps1lon <silbermann.sebastian@gmail.com>
2017-06-23 17:31:48 +0200

    implemented xml to json converter

---
+++ scripts/definitionsXmlToJson.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const xmlToJs = require('xml2js');
+
+const DAT_FILE = path.join(__dirname, '../data/DatDefinitions.xml');
+const OUT_FILE = path.join(__dirname, '../data/definitions.json');
+
+const definitions_file = fs.readFileSync(DAT_FILE);
+
+const parser = new xmlToJs.Parser();
+
+parser.parseString(definitions_file, (e, root) => {
+  if (e) {
+    throw e;
+  }
+
+  fs.writeFileSync(OUT_FILE, JSON.stringify(root.definitions.records, null, 2));
+});

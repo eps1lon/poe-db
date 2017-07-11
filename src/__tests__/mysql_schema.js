cfcf658909bbb1988223e7829cf41f3fde4bee56
eps1lon <silbermann.sebastian@gmail.com>
2017-07-11 16:48:38 +0200

    reintroduced hole schema test

--- src/__tests__/mysql_schema.js
+++ src/__tests__/mysql_schema.js
@@ -3,7 +3,7 @@ const MysqlSchema = require('../MysqlSchema');
 const spec = require('./spec.json');
 
 describe('MysqlTableSchema', () => {
-  it.skip('should match the db schema', () => {
+  it('should match the db schema', () => {
     expect(new MysqlSchema(spec).createAllQuery()).toMatchSnapshot();
   });
 });

b8008c1baa3e62af8cbbae6d24159b9299474eec
eps1lon <silbermann.sebastian@gmail.com>
2017-07-13 09:02:33 +0200

    remoed format reintroduce as post-hook to debug syntax errors

--- scripts/generateModels.js
+++ scripts/generateModels.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const generate = require('babel-core').transformFromAst;
-const format = require('prettier-eslint');
 
 const { entriesToObj, throwOnError } = require('../src/util');
 const SequelizeModel = require('../src/model/SequelizeModel');
@@ -15,12 +14,14 @@ const writeAst = async model => {
 
   fs.writeFile(
     path.join(__dirname, '../src/models/', model.name() + '.js'),
-    format({ text: generate(ast).code }),
+    generate(ast).code,
     throwOnError(),
   );
 };
 
-for (const [name, props] of Object.entries(spec)) {
+for (const [name, props] of Object.entries(spec).filter(
+  ([name]) => name === 'Mods.dat',
+)) {
   const model = new SequelizeModel(name, props);
 
   writeAst(model);

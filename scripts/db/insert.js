cf3caa701a924c26341166854f0a4a1da47ca111
eps1lon <silbermann.sebastian@gmail.com>
2017-07-14 09:58:49 +0200

    added time stats for insert

--- scripts/db/insert.js
+++ scripts/db/insert.js
@@ -6,6 +6,11 @@ const records = require('../../data/records.json');
 (async () => {
   const orm = orm_creator({ logging: false });
   const models = require('../../src/models')(orm);
+
+  const start_time = Date.now();
+
+  let total_insert_count = 0;
+
   try {
     for (const model of Object.values(models)) {
       const model_records = records[model.DAT_FILE];
@@ -37,15 +42,18 @@ const records = require('../../data/records.json');
       const inserted = await model.bulkCreate(records_as_obj, {
         ignoreDuplicates: true,
       });
+      const affected_rows = inserted.length;
 
-      console.log(`inserted ${inserted.length} instances into ${model.name}`);
+      total_insert_count += affected_rows;
+
+      console.log(`inserted ${affected_rows} instances into ${model.name}`);
 
       // create entries in the Through models
       for (const assoc in many_to_may_records) {
         const records = many_to_may_records[assoc];
         const assoc_model = model.associations[assoc].through.model;
 
-        const affected_rows = (await assoc_model.bulkCreate(
+        const inserted = await assoc_model.bulkCreate(
           records.map(([source, target]) => {
             return {
               [model.associations[assoc].foreignKey]: source,
@@ -53,7 +61,10 @@ const records = require('../../data/records.json');
             };
           }),
           { ignoreDuplicates: true },
-        )).length;
+        );
+        const affected_rows = inserted.length;
+
+        total_insert_count += affected_rows;
 
         console.log(
           `inserted ${affected_rows} associations into ${assoc_model.name}`,
@@ -64,5 +75,11 @@ const records = require('../../data/records.json');
     console.warn(e);
   } finally {
     orm.close();
+
+    const runtime = (Date.now() - start_time) / 1000;
+    console.log(
+      `inserted ${total_insert_count} in ${runtime}s (${total_insert_count /
+        runtime} inserts/s)`,
+    );
   }
 })();

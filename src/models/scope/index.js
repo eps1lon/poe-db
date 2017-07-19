eedbdf4c78b4b2e2ddc86b6ad43fb21e13c8805b
eps1lon <silbermann.sebastian@gmail.com>
2017-07-19 20:04:29 +0200

    remove hole extension only work with models, no need to access full sequelize instance

--- src/models/scope/index.js
+++ src/models/scope/index.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const { modelFiles } = require('../util');
 
-module.exports = sequelize => {
+module.exports = models => {
   for (const scope_file of modelFiles(__dirname)) {
-    const model_name = path.basename(scope_file, 'js');
-    const model = sequelize.models[model_name];
+    const model_name = path.basename(scope_file, '.js');
+    const model = models[model_name];
 
     if (model !== undefined) {
       const scopes = require(scope_file);
@@ -19,5 +19,5 @@ module.exports = sequelize => {
   // for chaining, would be nice to have it side-effect free
   // but the sequelize obj also includes a connection, 500+ models
   // so its prob better this way
-  return sequelize;
+  return models;
 };

f7dae7b82d23a778d6eee533036e455d45f01ef5
eps1lon <silbermann.sebastian@gmail.com>
2017-07-26 21:04:45 +0200

    enable cors with accept version

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -2,8 +2,9 @@ const restify = require('restify');
 
 const cors = () => {
   return (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Accept-Version');
+    res.setHeader('Access-Control-Max-Age', '1000');
 
     next();
   };
@@ -20,7 +21,13 @@ const createServer = () => {
   server.use(restify.plugins.queryParser());
   server.use(restify.plugins.bodyParser());
   server.use(restify.plugins.throttle({ burst: 10, rate: 1, ip: true }));
+
+  // enable cors, see https://github.com/restify/node-restify/issues/296
   server.use(cors());
+  server.opts('/.*/', (req, res, next) => {
+    res.send(200);
+    return next();
+  });
 
   return server;
 };

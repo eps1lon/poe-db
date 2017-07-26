61ff9bdf6c3b4f3f2d797d55700d4ff603cefd17
eps1lon <silbermann.sebastian@gmail.com>
2017-07-26 16:38:40 +0200

    added global rate limit

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -9,6 +9,7 @@ const createServer = () => {
   server.use(restify.plugins.acceptParser(server.acceptable));
   server.use(restify.plugins.queryParser());
   server.use(restify.plugins.bodyParser());
+  server.use(restify.plugins.throttle({ burst: 10, rate: 5, ip: true }));
 
   return server;
 };

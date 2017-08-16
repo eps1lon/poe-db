168574950284736459d655e9ffa428bf38eddcd1
eps1lon <silbermann.sebastian@gmail.com>
2017-08-16 15:47:02 +0200

    Removed db related tasks
    User should take care of this. Before that we encouraged usage of  a user with global rights

--- scripts/db/drop.js
+++
@@ -1,9 +0,0 @@
-const { connection, name } = require('../../src/db');
-
-const db = connection({ database: undefined });
-
-db.connect();
-
-db.query('DROP DATABASE IF EXISTS ??', [name]);
-
-db.end();

178a5b38a97595430a667694538bf9b74605669e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-13 21:35:02 +0200

    Added auth header to cors
    changed order of applied middleware. allowed cors headers were set after they were needed in previous middleware (accessKey)

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -7,7 +7,10 @@ const { mountRoutes } = require('./routes');
 const cors = () => {
   return (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Accept-Version');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Accept-Version, Authorization',
+    );
     res.setHeader('Access-Control-Max-Age', '1000');
 
     next();
@@ -23,6 +26,13 @@ const createServer = models => {
     version: '1.0.0',
   });
 
+  // enable cors, see https://github.com/restify/node-restify/issues/296
+  server.use(cors());
+  server.opts('/.*/', (req, res, next) => {
+    res.send(200);
+    return next();
+  });
+
   server.use(restify.plugins.acceptParser(server.acceptable));
   server.use(restify.plugins.queryParser());
   server.use(restify.plugins.bodyParser());
@@ -32,13 +42,6 @@ const createServer = models => {
   } else {
     require('./enhanceServer.dev')(server);
   }
-
-  // enable cors, see https://github.com/restify/node-restify/issues/296
-  server.use(cors());
-  server.opts('/.*/', (req, res, next) => {
-    res.send(200);
-    return next();
-  });
 
   // custom middleware
   server.use(exposeGameVersion);

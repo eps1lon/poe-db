69c1ac8e96e4e298cce865f8d7a77ea2414f0a86
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-11-17 11:36:59 +0100

    Prettier

--- src/models/util.js
+++ src/models/util.js
@@ -6,7 +6,10 @@ const BASE_PATH = path.join(__dirname, 'base');
 const isModelFile = file => /[A-Z].*\.js$/.test(file);
 
 const modelFiles = dir =>
-  fs.readdirSync(dir).filter(isModelFile).map(file => path.join(dir, file));
+  fs
+    .readdirSync(dir)
+    .filter(isModelFile)
+    .map(file => path.join(dir, file));
 
 const baseModelFiles = () => modelFiles(BASE_PATH);
 

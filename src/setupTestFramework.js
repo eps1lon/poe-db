4aa5c01de86c35650391681f2b4156a5832f98d9
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-03-03 14:16:19 +0100

    Support dotenv in jest tests

--- src/setupTestFramework.js
+++ src/setupTestFramework.js
@@ -1,3 +1,6 @@
 // Hack to make iconv load the encodings module, otherwise jest crashes. Compare
 // https://github.com/sidorares/node-mysql2/issues/489
 require('iconv-lite').encodingExists('cesu8');
+
+// load dotenv
+require('dotenv').config();
